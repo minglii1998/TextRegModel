50c345af7229f49ecc270312e24612b6af6a9476
minglii <minglii@foxmail.com>
2019-08-15 16:07:55 +0800

    model complete

--- resNet.py
+++ resNet.py
@@ -1,29 +1,19 @@
-import torch
 import torch.nn as nn
-
 import math
 
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-                     
 class Bottleneck(nn.Module):
     expansion = 4
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
 
-        self.conv1 = conv1x1(inplanes, planes*2, stride)
+        self.conv1 = nn.Conv2d(inplanes, planes*2, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes*2)
-        self.conv2 = conv3x3(planes*2, planes*2, stride)
+        self.conv2 = nn.Conv2d(planes*2, planes*2, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes*2)
-        self.conv3 = conv1x1(planes*2, planes*4, stride)
-        self.bn2 = nn.BatchNorm2d(planes*4)
+        self.conv3 = nn.Conv2d(planes*2, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -34,40 +24,42 @@ class Bottleneck(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
+
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
+
         out = self.conv3(out)
         out = self.bn3(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
-        
+
         out += residual
         out = self.relu(out)
 
         return out
 
+
 class ResNeXt(nn.Module):
 
-    def __init__(self, layers, num_classes=1000):
-        super(ResNeXt, self).__init__()
+    def __init__(self, block, layers, num_classes=1000):
         self.inplanes = 64
+        super(ResNeXt, self).__init__()
 
-        self.layer1 = nn.Sequential(
+        self.layer0 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
-
-        self.layer2 = self._make_layer(64, layers[0])
-        self.layer3 = self._make_layer(128, layers[1], stride=2)
-        self.layer4 = self._make_layer(256, layers[2], stride=2)
-        self.layer5 = self._make_layer(512, layers[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
 
         self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512 * Bottleneck.expansion, num_classes)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -77,29 +69,30 @@ class ResNeXt(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _make_layer(self, planes, blocks, stride=1):
+    def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
-        if stride != 1 or self.inplanes != planes * Bottleneck.expansion:
+        if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * Bottleneck.expansion, stride=stride),
-                nn.BatchNorm2d(planes * Bottleneck.expansion)
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
             )
-        
+
         layers = []
-        layers.append(Bottleneck(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * Bottleneck.expansion
-        for _ in range(1, blocks):
-            layers.append(Bottleneck(self.inplanes, planes))
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        
+
+        x = self.layer0(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        x = self.layer5(x)
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
@@ -107,9 +100,24 @@ class ResNeXt(nn.Module):
 
         return x
 
+
+
 def resnext50(**kwargs):
     """Constructs a ResNeXt-50 model.
     """
-    model = ResNeXt([3, 4, 6, 3], **kwargs)
-    return model   
+    model = ResNeXt(Bottleneck, [3, 4, 6, 3], **kwargs)
+    return model
 
+
+def resnext101(**kwargs):
+    """Constructs a ResNeXt-101 model.
+    """
+    model = ResNeXt(Bottleneck, [3, 4, 23, 3], **kwargs)
+    return model
+
+
+def resnext152(**kwargs):
+    """Constructs a ResNeXt-152 model.
+    """
+    model = ResNeXt(Bottleneck, [3, 8, 36, 3], **kwargs)
+    return model
